a1acd3046d1f487a944bbfa6be3f8b135ff23e01
aicemaria112 <aicemaria112@gmail.com>
2025-04-15 21:49:51 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 docker build -t whatsapp-bot .
-docker run -d --name whatsapp-bot-container -p 3000:3000 whatsapp-bot
+docker run -d --name whatsapp-bot-container \
+ -v db:/usr/src/app/data \
+ -v auth_data:/usr/src/app/.wwebjs_auth \
+ -p 3000:3000 whatsapp-bot
 docker inspect whatsapp-bot-container
